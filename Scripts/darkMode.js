419b84ba2f11ee9c92298975b2440be24766cf9c
magic-clicks <225111685+magic-clicks@users.noreply.github.com>
2026-01-25 11:29:23 -0600

    Added localstorage to save dark mode preference

--- Scripts/darkMode.js
+++ Scripts/darkMode.js
@@ -1,14 +1,26 @@
 const toggleButton = document.getElementById("dark-mode");
 const body = document.body;
- 
-    toggleButton.addEventListener(`click`, function(){
 
-        body.classList.toggle(`dark-mode`);
-        
-        if(body.classList.contains(`dark-mode`)){
-            toggleButton.textContent = `Light Mode`;
-        }
-        else{
-            toggleButton.textContent = `Dark Mode`;
-        }
-    })
+const savedTheme = localStorage.getItem("theme");
+
+if(savedTheme === "dark"){
+    body.classList.add("dark-mode");
+    toggleButton.textContent = "Light Mode";
+}
+else{
+    toggleButton.textContent = "Dark Mode";
+}
+ 
+toggleButton.addEventListener(`click`, function(){
+
+    body.classList.toggle(`dark-mode`);
+    
+    if(body.classList.contains(`dark-mode`)){
+        toggleButton.textContent = `Light Mode`;
+        localStorage.setItem("theme", "dark");
+    }
+    else{
+        toggleButton.textContent = `Dark Mode`;
+        localStorage.setItem("theme", "light");
+    }
+});
